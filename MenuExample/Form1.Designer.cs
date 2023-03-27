7c93e24ca626f442c3c7238e97be80dfc0bd5246
Егор Корсун <egorkorsun7@gmail.com>
2023-03-27 23:22:45 +0200

    Добавьте файлы проекта.

---
+++ MenuExample/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MenuExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopLevelMenu = new System.Windows.Forms.TextBox();
+            this.SubItem = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.AddTopLevelMenu = new System.Windows.Forms.Button();
+            this.AddSubItem = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TopLevelMenu
+            // 
+            this.TopLevelMenu.Location = new System.Drawing.Point(12, 35);
+            this.TopLevelMenu.Name = "TopLevelMenu";
+            this.TopLevelMenu.Size = new System.Drawing.Size(140, 22);
+            this.TopLevelMenu.TabIndex = 0;
+            // 
+            // SubItem
+            // 
+            this.SubItem.Location = new System.Drawing.Point(12, 63);
+            this.SubItem.Name = "SubItem";
+            this.SubItem.Size = new System.Drawing.Size(140, 22);
+            this.SubItem.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(551, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // AddTopLevelMenu
+            // 
+            this.AddTopLevelMenu.Location = new System.Drawing.Point(12, 92);
+            this.AddTopLevelMenu.Name = "AddTopLevelMenu";
+            this.AddTopLevelMenu.Size = new System.Drawing.Size(140, 23);
+            this.AddTopLevelMenu.TabIndex = 3;
+            this.AddTopLevelMenu.Text = "AddTopLevelMenu";
+            this.AddTopLevelMenu.UseVisualStyleBackColor = true;
+            this.AddTopLevelMenu.Click += new System.EventHandler(this.AddTopLevelMenu_Click_1);
+            // 
+            // AddSubItem
+            // 
+            this.AddSubItem.Location = new System.Drawing.Point(11, 121);
+            this.AddSubItem.Name = "AddSubItem";
+            this.AddSubItem.Size = new System.Drawing.Size(140, 23);
+            this.AddSubItem.TabIndex = 4;
+            this.AddSubItem.Text = "AddSubItem";
+            this.AddSubItem.UseVisualStyleBackColor = true;
+            this.AddSubItem.Click += new System.EventHandler(this.AddSubItem_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Меню";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Подменю";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 175);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddSubItem);
+            this.Controls.Add(this.AddTopLevelMenu);
+            this.Controls.Add(this.SubItem);
+            this.Controls.Add(this.TopLevelMenu);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Добавление меню";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TopLevelMenu;
+        private System.Windows.Forms.TextBox SubItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button AddTopLevelMenu;
+        private System.Windows.Forms.Button AddSubItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MenuExample/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace MenuExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void AddTopLevelMenu_Click_1(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = new ToolStripMenuItem(TopLevelMenu.Text);
+
+            // Добавляем пункт меню в главное меню
+            menuStrip1.Items.Add(menuItem);
+        }
+
+        private void AddSubItem_Click_1(object sender, EventArgs e)
+        {
+            int index = menuStrip1.Items.IndexOfKey(TopLevelMenu.Text);
+
+            // Если индекс равен -1, значит выбранный пункт меню не найден
+            if (index == -1)
+            {
+                MessageBox.Show("Выбранный пункт меню не найден.");
+                return;
+            }
+
+            // Получаем выбранный пункт меню
+            ToolStripMenuItem menuItem = (ToolStripMenuItem)menuStrip1.Items[index];
+
+            // Создаем новый подпункт меню
+            ToolStripMenuItem subMenuItem = new ToolStripMenuItem(SubItem.Text);
+
+            // Добавляем подпункт меню к выбранному пункту меню
+            menuItem.DropDownItems.Add(subMenuItem);
+        }
+    }
+}
